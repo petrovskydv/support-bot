e578c0b6136620e16d1afd6afbb6f939a129a983
Денис Петровский <petrovsky.dv@gmail.com>
2021-01-28 21:52:07 +0300

    подключил DialogFlow к ВКонтакте

--- vk_bot.py
+++ vk_bot.py
@@ -5,11 +5,13 @@ import vk_api as vk
 from dotenv import load_dotenv
 from vk_api.longpoll import VkLongPoll, VkEventType
 
+from utils import detect_intent_texts
 
-def echo(event, vk_api):
+
+def echo(event, vk_api, answer):
     vk_api.messages.send(
         user_id=event.user_id,
-        message=event.text,
+        message=answer,
         random_id=random.randint(1, 1000)
     )
 
@@ -17,12 +19,14 @@ def echo(event, vk_api):
 def main():
     load_dotenv()
     vk_token = os.environ['VK_TOKEN']
+    project_id = os.environ['GOOGLE_PROJECT_ID']
     vk_session = vk.VkApi(token=vk_token)
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api)
+            answer = detect_intent_texts(project_id, event.user_id, [event.text], 'ru')
+            echo(event, vk_api, answer)
 
 
 if __name__ == "__main__":
